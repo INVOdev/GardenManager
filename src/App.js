579dffeac0a00a78ec09e632a2ee89b699da4a34
boorish92@gmail.com <boorish92@gmail.com>
2017-06-28 23:13:06 +0200

    firebase connection

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fire from './fire';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      showText: true,
+      messages: []
+    };
+  }
+  componentWillMount(){
+    // Loop through users in order with the forEach() method. The callback
+  // provided to forEach() will be called synchronously with a DataSnapshot
+  // for each child:
+  console.log('get data');
+  var query = fire.database().ref("data/garden").orderByKey();
+  query.once("value")
+    .then(function(snapshot) {
+      // console.log(snapshot.val());
+      snapshot.forEach(function(childSnapshot) {
+        console.log('key: ' + childSnapshot.key);
+        // childData will be the actual contents of the child
+        console.log(childSnapshot.val());
+      });
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
