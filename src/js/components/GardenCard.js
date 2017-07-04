68a8a77b8995e198af1b328e7965a353559f97dd
boorish92@gmail.com <boorish92@gmail.com>
2017-07-04 15:59:02 +0200

    card not rwd

--- src/js/components/GardenCard.js
+++ src/js/components/GardenCard.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Col, Card } from 'antd';
+
 import 'antd/lib/card/style/css';
 import 'antd/lib/col/style/css';
-
 import '../../scss/gardenCard.scss';
 
 class GardenCard extends Component {
@@ -15,16 +15,14 @@ class GardenCard extends Component {
 
   render(){
     let content = this.props.content;
-
     return(
-      <Col span={6}>
+      <Col span={6} xs={24}>
         <div className="gardenCard">
-          <Card style={{ width: 300 }} bodyStyle={{ padding: 0 }}>
+          <Card title={content.key} style={{ width: 300 }} bodyStyle={{ padding: 0 }}>
             <div className="custom-image">
               <img alt="example" width="100%" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
             </div>
             <div className="custom-card">
-              <h3>{content.key}</h3>
               <p> {content.name} </p>
               <div> {content.map} </div>
               <div> {content.plants} </div>
@@ -35,5 +33,4 @@ class GardenCard extends Component {
     );
   }
 }
-
 export default GardenCard;
